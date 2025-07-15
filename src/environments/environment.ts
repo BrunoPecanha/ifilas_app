35728a1fa35144670b0c7c4252249e2f7f2ef9f7
Bruno Peçanha <brunoitdesenv@gmail.com>
2025-07-15 00:52:48 -0300

    🚧 Ajuste do enviroment 🚧 Contador de pessoas no botão de fila

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
-export const environment = {
-   production: false,
-   apiUrl: 'https://localhost:5001/api',  
-   queueHub: 'https://localhost:5001/queueHub',
-   notificationHub: 'https://localhost:5001/notificationHub',
+ export const environment = {
+    production: false,
+    apiUrl: 'https://localhost:5001/api',  
+    queueHub: 'https://localhost:5001/queueHub',
+    notificationHub: 'https://localhost:5001/notificationHub'
 };
