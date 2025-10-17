034cc702ac38566cb0283493732b534765c33226
Bruno Peçanha <brunoitdesenv@gmail.com>
2025-10-17 02:34:39 -0300

    nextu - 149 - inclui leitura de dados do backend e formata em tela

---
+++ src/services/dashboard.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { environment } from "src/environments/environment";
+import { DashBoardResponse } from "src/models/responses/dashboard-response";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DashBoardService {
+  private apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  loadCustomerInfo(id: number): Observable<DashBoardResponse> {
+    return this.http.get<DashBoardResponse>(`${this.apiUrl}/dashboard/${id}`);
+  } 
+}
